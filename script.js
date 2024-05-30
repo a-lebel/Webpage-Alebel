f7fe003118dd196423d6a9bd06a9fefd521ae9cf
alebel <alejandralebel@gmail.com>
2024-05-30 20:41:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+desarrollo.addEventListener("mouseover", () => {
+    desarrollo.style.color = "#A1998A";
+});
+
+desarrollo.addEventListener("mouseleave", () => {
+    desarrollo.style.color = "#f6f6f6";
+});
+
+web.addEventListener("mouseover", () => {
+    web.style.color = "#A1998A";
+    web.style.fontSize = "14rem";
+});
+
+web.addEventListener("mouseleave", () => {
+    web.style.color = "";
+    web.style.fontSize = "";
+});
+
